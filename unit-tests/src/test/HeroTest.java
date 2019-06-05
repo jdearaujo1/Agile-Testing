cfe23ed2a8ab62aa84c07f5700f87c1f84b90a00
Unknown <yanis88357@gmail.com>
2019-06-05 09:58:19 +0200

    testAttack et testSetLevel

--- unit-tests/src/test/HeroTest.java
+++ unit-tests/src/test/HeroTest.java
@@ -1,20 +1,24 @@
 package test;
 
+import junit.framework.*;
 import org.junit.Test;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
-
+import static org.junit.Assert.*;
 import static org.junit.Assert.assertTrue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 import codingfactory.rpgconsole.hero.Hero;
+import codingfactory.rpgconsole.enemy.Enemy;
 
+import java.util.Random;
 public class HeroTest {
 
 	Hero hero;
+	Enemy enemy;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -29,6 +33,7 @@ public class HeroTest {
 	@Before
 	public void setUp() throws Exception {
 		hero = new Hero("Jaina Portvaillant");
+		enemy = new Enemy("Skeleton", 1);
 		System.out.println("Avant un test");
 	}
 
@@ -49,4 +54,14 @@ public class HeroTest {
         assertThat(hero, hasProperty("name", is("Jaina Portvaillant")));
 	}
 
-}
+	@Test
+	public void testAttack() throws Exception {
+		Random rand = new Random();
+		int randomNum = rand.nextInt(hero.getLevel() + 1);
+		assertThat(hero.getAtk() + randomNum, greaterThanOrEqualTo(0));
+	}
+
+	@Test
+	public void testSetLevel() throws Exception {
+		assertThat(hero.getLevel(), greaterThan(0));
+	}
