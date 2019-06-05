8cdf16e07343da8a27bfc21ccd211d8810d2cc47
Jordan <jordan.dearaujo@edu.itescia.fr>
2019-06-05 10:24:45 +0200

    Update compileAndRun
    Add EnemyTest

--- unit-tests/compileAndRun.sh
+++ unit-tests/compileAndRun.sh
@@ -12,9 +12,14 @@ echo 'Game compiled successfully ✓'
 javac -d ../bin/ test/HeroTest.java
 echo 'HeroTest compiled successfully ✓'
 
+javac -d ../bin/ test/EnemyTest.java
+echo 'EnemyTest compiled successfully ✓'
+
 
 printf 'Running HeroTest ... \n\n'
 
 
 cd ../bin/
 java org.junit.runner.JUnitCore test.HeroTest
+java org.junit.runner.JUnitCore test.EnemyTest
+
